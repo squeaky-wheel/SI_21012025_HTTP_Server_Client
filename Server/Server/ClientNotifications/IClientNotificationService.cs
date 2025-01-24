ec1eb6311eaea5e97b85ba9f5e9e18a4ddb0c20e
squeaky-wheel <112768719+squeaky-wheel@users.noreply.github.com>
2025-01-24 15:14:43 +0000

    Fixed a typo. Amended out-of-date comments.

--- Server/Server/ClientNotifications/IClientNotificationService.cs
+++ Server/Server/ClientNotifications/IClientNotificationService.cs
@@ -5,13 +5,13 @@ namespace Server.ClientNotifications
     public interface IClientNotificationService
     {
         /// <summary>
-        /// Sets a socket for use by the service. The socket has to be remove using <see cref="RemoveSocketAsync"/>
+        /// Sets a socket for use by the service. The socket has to be removed using <see cref="RemoveSocketAsync"/>
         /// once no longer needed. The service can have only one socket set at a given time.
         /// </summary>
         /// <exception cref="ArgumentNullException">When <paramref name="socketToSet"/> is absent.</exception>
         /// <exception cref="OperationCanceledException">When socket setting has been cancelled.</exception>
         /// <exception cref="InvalidOperationException">When the service already has a socket.</exception>
-        /// <exception cref="ObjectDisposedException">When the service has been disposed.</exception>
+        /// <exception cref="ObjectDisposedException">When the synchronisation mechanisms have been disposed.</exception>
         Task SetSocketAsync(WebSocket socketToSet, CancellationToken cancellationToken);
 
         /// <summary>
@@ -21,7 +21,7 @@ namespace Server.ClientNotifications
         /// A socket must have been set using <see cref="SetSocketAsync"/>
         /// </summary>
         /// <exception cref="InvalidOperationException">When an attempt is made to remove an absent socket.</exception>
-        /// <exception cref="ObjectDisposedException">When the service has been disposed.</exception>
+        /// <exception cref="ObjectDisposedException">When the synchronisation mechanisms have been disposed.</exception>
         Task RemoveSocketAsync();
 
         /// <returns><see cref="true"/> if the closure request was received;
@@ -29,7 +29,7 @@ namespace Server.ClientNotifications
         /// <exception cref="WebSocketException">When an attempt to receive a closure request resulted in
         /// an unexpected boundary condition.</exception>
         /// <exception cref="OperationCanceledException">When the wait has been cancelled.</exception>
-        /// <exception cref="ObjectDisposedException">When the service has been disposed.</exception>
+        /// <exception cref="ObjectDisposedException">When the synchronisation mechanisms have been disposed.</exception>
         Task<bool> WaitForClosureRequestAsync(CancellationToken cancellationToken);
 
         /// <returns><see cref="true"/> if the <paramref name="text"/> was transmitted;
@@ -37,7 +37,7 @@ namespace Server.ClientNotifications
         /// <exception cref="ArgumentException">When <paramref name="text"/> is either absent,
         /// empty or consists of white-space only..</exception>
         /// <exception cref="OperationCanceledException">When the trasmission has been cancelled.</exception>
-        /// <exception cref="ObjectDisposedException">When the service has been disposed.</exception>
+        /// <exception cref="ObjectDisposedException">When the synchronisation mechanisms have been disposed.</exception>
         Task<bool> TransmitAsync(string text, CancellationToken cancellationToken);
     }
 }
